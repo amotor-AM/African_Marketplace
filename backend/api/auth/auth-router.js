fd2aa1843ec0b7a02037ad805cccd047b3885d84
Brandon Maroni <63432771+Bmmaroni@users.noreply.github.com>
2021-03-25 17:45:34 -0600

    auth changes

--- backend/api/auth/auth-router.js
+++ backend/api/auth/auth-router.js
@@ -5,7 +5,19 @@ const Auth = require("./auth-model")
 
 router.post("/register", async (req, res, next) => {
 	try {
+		const { username, password } = req.body
 
+		if(!username || !password){
+			return res.status(400).json({
+				message: "username and password required"
+			})
+		} else {
+			const newUser = await Auth.add({
+				username: username,
+				password: await bcrypt.hash(password, 5)
+			})
+			res.status(201).json(newUser)
+		}
 	} catch (err) {
 		next(err)
 	}
@@ -13,6 +25,32 @@ router.post("/register", async (req, res, next) => {
 
 router.post("/login", async (req, res, next) => {
 	try {
+		const { username, password } = req.body
+		if(!username || !password){
+			return res.status(400).json({
+				message: "username and password required"
+			})
+		}
+		const user = await Auth.findBy({ username }).first()
+		const checkPassword = await bcrypt.compare(password, user ? user.password : "")
+
+		if (!user || !checkPassword){
+			return res.status(401).json({
+				message: "username or password incorrect"
+			})
+		} else {
+			const token = jwt.sign({
+				subject: user.id,
+				username: user.username
+			}, "keep it secret keep it safe", {expiresIn: "1d"})
+			
+			res.cookie("token", token)
+
+			res.status(200).json({
+				message: `Welcome back ${username}!`,
+				token: token
+			})
+		}
 		
 	} catch (err) {
 		next(err)
@@ -21,6 +59,13 @@ router.post("/login", async (req, res, next) => {
 
 router.post("/logout", async (req, res, next) => {
 	try {
+		req.session.destroy((err) => {
+			if (err){
+				next(err)
+			} else {
+				res.status(204).end()
+			}
+		})
 		
 	} catch (err) {
 		next(err)
