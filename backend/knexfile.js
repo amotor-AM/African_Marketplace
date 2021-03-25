dca8d95d44b98347f86c29296891db6fe7ad640b
amotor-AM <66324211+amotor-AM@users.noreply.github.com>
2021-03-24 18:35:48 -0700

    updated file

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -24,10 +24,10 @@ module.exports = {
   production: {
     ...sharedConfig,
     // connection: process.env.DATABASE_URL,
-    // pool: { min: 2, max: 10 },
+    pool: { min: 2, max: 10 },
     // connectionString: pgConnection
     connection: {
-      connectionString: pgConnection,
+      connectionString: process.env.DATABASE_URL,
       ssl: {rejectUnauthorized: false}
     }
   },
