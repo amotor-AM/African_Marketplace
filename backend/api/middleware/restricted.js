c898282028d0eb3e6bb4ac7cfdde3fd3d7a684f8
Brandon Maroni <63432771+Bmmaroni@users.noreply.github.com>
2021-04-01 19:20:57 -0600

    updated middleware for front end use

--- backend/api/middleware/restricted.js
+++ backend/api/middleware/restricted.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken")
 
 const restricted = async (req, res, next) => {
 	try{
-		const token = req.cookies.token
+		const token = req.headers.authorization
 		if (!token){
 			return res.status(401).json({
 				message: "token required"
